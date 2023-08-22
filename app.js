762e82a07a81f63f02d4d2834d7295ae3be1da9a
Yoariht Macedo <yoariht.mc37@gmail.com>
2023-08-21 18:40:01 -0600

    hola mundo en ts

---
+++ app.js
@@ -0,0 +1,5 @@
+var msg = 'Hola mundo';
+console.log(msg);
+// Para generar la versión de js del archivo se debe escribir el comando
+// tsc app que genera el archivo .js pero debemos situarnos en la carpeta
+// donde se encuentra el archivo de ts
---
+++ app.ts
@@ -0,0 +1,9 @@
+
+
+const msg: string = 'Hola mundo';
+
+console.log(msg);
+
+// Para generar la versión de js del archivo se debe escribir el comando
+// tsc app que genera el archivo .js pero debemos situarnos en la carpeta
+// donde se encuentra el archivo de ts
